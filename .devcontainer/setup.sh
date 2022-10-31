25d85581b16d3b9e0818d7e8145ff9ee46ae8342
Loaki07 <66478092+Loaki07@users.noreply.github.com>
2022-10-31 12:07:41 +0530

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,14 @@
-# update and install some things we should probably have
+## update and install some things we should probably have
 apt update
 
 ## Install rustup and common components
 rustup update
+rustup default stable
 rustup update nightly
 rustup component add rustfmt
 rustup component add rustfmt --toolchain nightly
 rustup component add clippy 
 rustup component add clippy --toolchain nightly
-rustup default nightly
+rustup install nightly-2022-02-15
+rustup default nightly-2022-02-15
 rustup target add wasm32-unknown-unknown
